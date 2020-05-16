41efffe4df513b3544983f3286744672d9e6c13e
lweitkamp <laurens.weitkamp@student.uva.nl>
2020-05-16 09:58:52 +0200

    Adding dilated LSTM fix

--- dilated_lstm.py
+++ dilated_lstm.py
@@ -3,39 +3,42 @@ import torch.nn as nn
 
 
 class DilatedLSTM(nn.Module):
-    """
-    self.index: a simple arange used for indexing the hx, cx tensors.
-    self.dilation: incremental dilation index to be added to self.index
-    """
-
-    def __init__(self, input_size, hidden_size, r=10):
+    def __init__(self, input_size, hidden_size, radius=10):
         super().__init__()
-        self.r = r
+        self.radius = radius
+        self.hidden_size = hidden_size
         self.rnn = nn.LSTMCell(input_size, hidden_size)
-        self.index = torch.arange(0, r * hidden_size, r)
+        self.index = torch.arange(0, radius * hidden_size, radius)
         self.dilation = 0
 
     def forward(self, state, hidden):
-        d_idx = self.dilation + self.index
-        self.dilation = (self.dilation + 1) % self.r
-
+        """At each time step only the corresponding part of the state is updated
+        and the output is pooled across the previous c out- puts."""
+        d_idx = self.dilation_idx
         hx, cx = hidden
-        hx_, cx_ = self.rnn(state, (hx[:, d_idx], cx[:, d_idx]))
-        hx[:, d_idx] = hx_
-        cx[:, d_idx] = cx_
 
-        return hx[:, d_idx], (hx, cx)
+        hx[:, d_idx], cx[:, d_idx] = self.rnn(state, (hx[:, d_idx], cx[:, d_idx]))
+        detached_hx = hx[:, self.masked_idx(d_idx)].detach()
+        detached_hx = detached_hx.view(detached_hx.shape[0], self.hidden_size, self.radius-1)
+        detached_hx = detached_hx.sum(-1)
 
+        y = (hx[:, d_idx] + detached_hx) / self.radius
+        return y, (hx, cx)
 
-if "__main__" == __name__:
-    r = 4
+    def masked_idx(self, dilated_idx):
+        """Because we do not want to have gradients flowing through all
+        parameters but only at the dilation index, this function creates a
+        'negated' version of dilated_index, everything EXCEPT these indices."""
+        masked_idx = torch.arange(1, self.radius * self.hidden_size + 1)
+        masked_idx[dilated_idx] = 0
+        masked_idx = masked_idx.nonzero()
+        masked_idx = masked_idx - 1
+        return masked_idx
 
-    d_LSTM = DilatedLSTM(256, 64, r)
-    hx = cx = torch.zeros(5, r * 256, requires_grad=True)
-    hidden = (hx, cx)
+    @property
+    def dilation_idx(self):
+        """Keep track at which dilation we currently we are."""
+        dilation_idx = self.dilation + self.index
+        self.dilation = (self.dilation + 1) % self.radius
+        return dilation_idx
 
-    for _ in range(11):
-        x = torch.ones(5, 256)
-        output, hidden = d_LSTM(x, hidden)
-        assert output.requires_grad is hidden[0].requires_grad \
-            is hidden[1].requires_grad is True, "Gradient not working."
